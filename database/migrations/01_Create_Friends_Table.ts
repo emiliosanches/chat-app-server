09c08a01d722bb9a5e1df1a97e6563208f34d800
Shadow584 <55329280+Shadow584@users.noreply.github.com>
2020-10-02 00:53:04 -0300

    Adding TBFriends to migrations

---
+++ database/migrations/01_Create_Friends_Table.ts
@@ -0,0 +1,28 @@
+import Knex from "knex";
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('TBFriends', table => {
+        table.increments('id').notNullable().primary();
+
+
+        table.string('user1')
+            .notNullable()
+            .references('username')
+            .inTable('TBUsers')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        
+        
+        table.string('user2')
+            .notNullable()
+            .references('username')
+            .inTable('TBUsers')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+
+    })
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('TBFriends')
+}
